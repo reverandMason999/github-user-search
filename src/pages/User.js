2703305dde6cc9c7b40645152a565657204c21d6
Mason.S98 <120329380+reverandMason999@users.noreply.github.com>
2023-05-04 18:01:40 -0400

    updated user page

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom"
+
+
 const User = () => {
+    const { userId } = useParams();
+
+    const selectedUser = useSelector((state) =>
+        state.users.value.find((user) => user.id === Number(userId))  
+    );
+    
     return (
-        <h1>a user</h1>
-    )
-}
+<div className="user">
+    <h1 className="username">{selectedUser.login}</h1>
+    <img className="user-avatar" src={selectedUser.avatar_url} alt="user avatar"/>
+    <h2 className="username">{selectedUser.name}</h2>
+    <p>{selectedUser.location || 'The Moon'}</p>
+</div>    );
+};
 
 export default User
